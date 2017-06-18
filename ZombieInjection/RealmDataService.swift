d887b9e6af97c47e49703b433f8231795217556f
Patrick Lind <pklind0728@gmail.com>
2017-06-18 11:59:17 -0400

    Created the RealmDataService for use in Repositories that persist their data into a Realm database.

---
+++ ZombieInjection/RealmDataService.swift
@@ -0,0 +1,96 @@
+//
+//  RealmDataService.swift
+//  ZombieInjection
+//
+//  Created by Patrick Lind on 6/13/17.
+//  Copyright © 2017 Patrick Lind. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmDataService<ItemType: Persistable>: DataServiceProtocol where ItemType: Object {
+    
+    let realm = try! Realm()
+    
+    func get(_ id: Int) -> ItemType? {
+        let predicate = NSPredicate(format: "id == %@", id)
+        return realm.objects(ItemType.self).filter(predicate).first
+    }
+    
+    func get(_ predicate: (ItemType) throws -> Bool) -> ItemType? {
+        guard let itemToGet = try? realm.objects(ItemType.self).filter(predicate).first else {
+            return nil
+        }
+        return itemToGet
+    }
+    
+    func getAll() -> Array<ItemType>? {
+        let results = Array(realm.objects(ItemType.self))
+        return results
+    }
+    
+    func getAll(_ predicate: (ItemType) throws -> Bool) -> Array<ItemType>? {
+        guard let results = try? realm.objects(ItemType.self).filter(predicate) else {
+            return nil
+        }
+        return Array(results)
+    }
+    
+    func contains(_ predicate: (ItemType) throws -> Bool) -> Bool {
+        guard let isPresent = try? realm.objects(ItemType.self).contains(where: predicate) else {
+            return false
+        }
+        return isPresent
+    }
+    
+    func insert(_ item: ItemType) {
+        try? realm.write {
+            realm.add(item)
+        }
+    }
+    
+    func insertAll(_ itemsToInsert: Array<ItemType>) {
+        try? realm.write {
+            realm.add(itemsToInsert)
+        }
+    }
+    
+    func delete(_ item: ItemType) {
+        try? realm.write {
+            realm.delete(item)
+        }
+    }
+    
+    func deleteAll(_ predicate: (ItemType) throws -> Bool) {
+        guard let itemsToDelete = try? realm.objects(ItemType.self).filter(predicate) else {
+            return
+        }
+        try? realm.write {
+            realm.delete(itemsToDelete)
+        }
+    }
+    
+    func deleteAll() {
+        try? realm.write {
+            realm.deleteAll()
+        }
+    }
+    
+    func update(_ item: ItemType) {
+        try? realm.write {
+            realm.add(item, update: true)
+        }
+    }
+    
+    func count() -> Int {
+        return realm.objects(ItemType.self).count
+    }
+    
+    func count(_ predicate: (ItemType) throws -> Bool) -> Int {
+        guard let count = try? realm.objects(ItemType.self).filter(predicate).count else {
+            return 0
+        }
+        return count
+    }
+}
