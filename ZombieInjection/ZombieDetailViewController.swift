baa05c4b618cd3e12cbd58a5009e83b5377aae74
Patrick Lind <pklind0728@gmail.com>
2017-07-11 23:42:41 -0400

    Updated to work with Realm.

--- ZombieInjection/ZombieDetailViewController.swift
+++ ZombieInjection/ZombieDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Bond
+import RealmSwift
 import UIKit
 
 class ZombieDetailViewController: UIViewController {
@@ -16,23 +17,41 @@ class ZombieDetailViewController: UIViewController {
     @IBOutlet weak var zombieNameTextField: UITextField!
     
     @IBAction func saveAction(_ sender: AnyObject) {
-        viewModel.save()
+        viewModel.save(unmanagedZombie: unmanagedDetailZombie)
+        // Make unmanagedDetailZombie unmanaged again otherwise will get Realm object update errors.
+        unmanagedDetailZombie = Zombie(value: unmanagedDetailZombie)
     }
     
     var viewModel: ZombieDetailViewModel!
+    var unmanagedDetailZombie: Zombie!
     
     // MARK: View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        viewModel.zombie.image.bind(to: zombieImageView.reactive.image).dispose(in: reactive.bag)
-        viewModel.zombie.observableName.bidirectionalBind(to: zombieNameTextField.reactive.text).dispose(in: reactive.bag)
+        unmanagedDetailZombie = Zombie(value: self.viewModel.zombie)
+        self.viewModel.zombie.image.bind(to: zombieImageView.reactive.image).dispose(in: reactive.bag)
+        bindZombieName()
     }
     
     // Need to get rid of the view model to prevent a memory leak.
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         viewModel = nil
+    }
+    
+    private func bindZombieName() {
+        _ = self.unmanagedDetailZombie?.observableName.observeNext {
+            event in
+            self.zombieNameTextField.text = event
+        }
+        
+        _ = zombieNameTextField.reactive.text.observeNext {
+            event in
+            if let event = event {
+                self.unmanagedDetailZombie?.observableName = Observable(event)
+            }
+        }
     }
 }
